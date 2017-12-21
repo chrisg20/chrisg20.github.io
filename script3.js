4e6d65710dd9f83facb7bc69cc0e8d66caafe3e6
chrisg20 <31445708+chrisg20@users.noreply.github.com>
2017-12-21 10:38:28 -0700

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,14 @@
+var correct = Math.floor(Math.random() * 6);
+var guess = prompt("Enter a password between 0 and 5 inclusive.")
+var tries = 1;
+      
+while (guess != correct) {
+        
+   guess = prompt("The password is not " + guess + ", Try again.")
+      tries ++;
+         }
+      
+   else {
+      alert(" + guess + ", is correct. It took you " + tries + " tries!");
+        }
+          
